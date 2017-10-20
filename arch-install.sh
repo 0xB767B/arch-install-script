787b9deed873c4d13e7d0d69e4f93fa91560161e
Marco Schuler <marco.schuler@sensirion.com>
2017-10-20 09:28:44 +0000

    Refactor arch-install-script.sh

--- arch-install.sh
+++ arch-install.sh
@@ -5,101 +5,134 @@ set -e
 ################################################################################
 # Common functions
 ################################################################################
+
+configure_keyboard() {
+	loadkeys de_CH-latin1
+}
+
+unmount_partitions () {
+# Unmount partitions if alread mounted
+mounted_partitions=(`lsblk | grep "/mnt" | awk '{print $7}' | sort -r`)
+	for i in ${mounted_partitions[@]}; do
+		umount $i
+	done
+	
+	swapoff -a
+}
+
+configure_mirrorlist() {
+	# This step is currently not implemented since we use the default mirrorlist
+	# which should be good enough
+	echo "Configuration of mirrorlist not implemented yet."
+}
+
+create_partitions() {
+	# Create partitions 
+	# Here we create three partitions:
+	#  200 MB for UEFI
+	# 8192 MB for Swap
+	# Rest for root
+	# todo: use variable for device!
+	sgdisk -n 0:0:+200M -t 0:ef00 -c 0:"EFIBOOT" /dev/sda
+	sgdisk -n 0:0:+1G -t 0:8200 -c 0:"SWAP" /dev/sda
+	sgdisk -n 0:0:0 -t 0:8300 -c 0:"ROOT" /dev/sda
+}
+
+format_partitions() {
+	# Format partitions
+	mkfs.fat -F 32 -n "EFIBOOT" /dev/sda1
+	mkswap -L SWAP /dev/sda2
+	mkfs.ext4 -L ROOT /dev/sda3
+}
+
+mount_partitions() {
+	# Mount partitions
+	mount -L ROOT /mnt
+	mkdir -p /mnt/boot
+	mount -L EFIBOOT /mnt/boot
+	swapon -L SWAP
+}
+
+install_system() {
+	pacstrap /mnt base base-devel efibootmgr dosfstools gptfdisk
+}
+
+generate_fstab() {
+	genfstab -U /mnt >> /mnt/etc/fstab
+}
+
 arch_chroot() {
 	arch-chroot /mnt /bin/bash -c "${1}"
+}
+
+set_timezone() {
+	arch_chroot "ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime"
+}
+
+generate_adjtime() {
+	cp locale.gen /mnt/etc/
+	arch_chroot "locale-gen"
+	cp locale.conf /mnt/etc
+}
+
+set_keyboard_layout() {
+	cp vconsole.conf /mnt/etc
+}
+
+configure_hostname() {
+	cp hostname /mnt/etc/
+}
+
+configure_hosts() {
+	cp hosts /mnt/etc
+}
+
+create_initramfs() {
+	arch_chroot "mkinitcpio -p linux"
+}
+
+set_root_password() {
+	# todo
+	echo "Setting of root password not implemented yet."
+}
+
+install_bootloader() {
+	arch_chroot "bootctl install"
+	cp arch-uefi.conf /mnt/boot/loader/entries
+	cp arch-uefi-fallback.conf /mnt/boot/loader/entries
+	cp loader.conf /mnt/boot/loader
+}
+
+installation_finished() {
+	echo "It is now time for reboot"
+	echo "Type reboot"
 }
 
 ################################################################################
 # Basic setup
 ################################################################################
 
-# Set keyboard layout
-loadkeys de_CH-latin1
-
-# Install vim just in case we have to configure something during installation
-#pacman -S --noconfirm --needed vim git
-
-# Configure mirrorlist
-# This step is currently not implemented since we use the default mirrorlist
-# which should be good enough
-
-# Unmount partitions if alread mounted
-mounted_partitions=(`lsblk | grep "/mnt" | awk '{print $7}' | sort -r`)
-for i in ${mounted_partitions[@]}; do
-	umount $i
-done
-
-swapoff -a
-
-# Create partitions 
-# Here we create three partitions:
-#  200 MB for UEFI
-# 8192 MB for Swap
-# Rest for root
-# todo: use variable for device!
-sgdisk -n 0:0:+200M -t 0:ef00 -c 0:"EFIBOOT" /dev/sda
-sgdisk -n 0:0:+1G -t 0:8200 -c 0:"SWAP" /dev/sda
-sgdisk -n 0:0:0 -t 0:8300 -c 0:"ROOT" /dev/sda
-
-# Format partitions
-mkfs.fat -F 32 -n "EFIBOOT" /dev/sda1
-mkswap -L SWAP /dev/sda2
-mkfs.ext4 -L ROOT /dev/sda3
-
-# Mount partitions
-mount -L ROOT /mnt
-mkdir -p /mnt/boot
-mount -L EFIBOOT /mnt/boot
-swapon -L SWAP
-
-# Install system
-pacstrap /mnt base base-devel efibootmgr dosfstools gptfdisk
-
-# Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+configure_keyboard
+unmount_partitions
+configure_mirrorlist
+create_partitions
+format_partitions
+mount_partitions
+install_system
+generate_fstab
 
 ################################################################################
 # Basic setup in chroot
 ################################################################################
 
-# Set timezone
-arch_chroot "ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime"
-
-# Run hwclock to generate /etc/adjtime
-arch_chroot "hwclock --systohc"
-
-# Locale
-cp locale.gen /mnt/etc/
-arch_chroot "locale-gen"
-cp locale.conf /mnt/etc
-
-# Set keyboard layout
-cp vconsole.conf /mnt/etc
-
-# Hostname
-cp hostname /mnt/etc/
-cp hosts /mnt/etc
-
-# Create initramfs
-arch_chroot "mkinitcpio -p linux"
-
-# Password for root
-# todo
-
-# Install bootloader
-arch_chroot "bootctl install"
-cp arch-uefi.conf /mnt/boot/loader/entries
-cp arch-uefi-fallback.conf /mnt/boot/loader/entries
-cp loader.conf /mnt/boot/loader
-
-# Unmount partitions
-mounted_partitions=(`lsblk | grep "/mnt" | awk '{print $7}' | sort -r`)
-for i in ${mounted_partitions[@]}; do
-	umount $i
-done
-
-swapoff -a
-
-echo "It is now time for reboot"
-echo "Type reboot"
+set_timezone
+generate_adjtime
+set_keyboard_layout
+configure_hostname
+configure_hosts
+create_initramfs
+set_root_password
+install_bootloader
+unmount_partitions
+installation_finished
 
